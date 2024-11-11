3e2f85c3b10a9f3b96e37a65cd4f2f1cc326f668
Reshmi Ramesh <reshmiramesh07@gmail.com>
2024-11-11 10:06:42 -0800

    weapons list, player_setup func

--- demon-duels.py
+++ demon-duels.py
@@ -1,7 +1,18 @@
 class Player:
-    def __init__(self, name, weapon):
+    def __init__(self, name, weapon, won, total):
         self.name = name
         self.weapon = weapon
+        self.won = won
+        self.total = total
+
+player = Player("Player", "Sword", 0, 0)
+weapons = ["Sword", "Dagger", "Javelin", "Bow and arrows", "Staff"]
 
 def player_setup():
-    pass
+    player.name = input("What is your name?: ")
+    print("We have a lot to offer from our armory! As you gain more victories, more options will reveal themselves.")
+    # for loop print each weapon
+
+# functions - change_weapon, change_armor?
+
+player_setup()
