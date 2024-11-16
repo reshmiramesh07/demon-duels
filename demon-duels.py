203d7e07ad3fb7500217b7751a593fb1454badf9
Reshmi Ramesh <reshmiramesh07@gmail.com>
2024-11-16 13:44:22 -0800

    demon class, first demon's mechanics

--- demon-duels.py
+++ demon-duels.py
@@ -5,14 +5,48 @@ class Player:
         self.won = won
         self.total = total
 
+class Demon:
+    def __init__(self, name, hp, basic, charged, spec, rate, defeat):
+        self.name = name
+        self.hp = hp
+        self.basic = basic
+        self.charged = charged
+        self.spec = spec # spec for special move
+        self.rate = rate # difficulty of duel
+        self.defeat = defeat
+
+# creating player object
 player = Player("Player", "Sword", 0, 0)
 weapons = ["Sword", "Dagger", "Javelin", "Bow and arrows", "Staff"]
 
+# creating demon objects
+poltergiest = Demon("Poltergiest", 50, "Throw", "Destroy", "Tornado", "Easy", 0) # destroy will destory player's weapon; player atk goes down 50%
+all_demons = []
+
+def poltergiest_basic():
+    pass
+
+def poltergiest_charged():
+    pass
+
+def poltergiest_spec():
+    pass
+
+def battle_poltergiest():
+    pass
+
 def player_setup():
     player.name = input("What is your name?: ")
+    
+def battle_select(): # or choose weapon here
+    print("Pick your battle.")
+    # pick battle print and input here
+    print("Entering armory...")
     print("We have a lot to offer from our armory! As you gain more victories, more options will reveal themselves.")
-    # for loop print each weapon
+    for i in range(len(weapons)):
+        print("- " + weapons[i])
+    player.weapon = input("Choose your weapon:") # babyproof this
 
-# functions - change_weapon, change_armor?
+
 
 player_setup()
